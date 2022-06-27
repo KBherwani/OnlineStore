991a9b2ae9c520ff8b80fe8660baf5bf774ba402
KBherwani <kunalbherwani67@gmail.com>
2022-06-27 23:21:02 +0530

    login/logout

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import SignUpView, ActivateAccount
+from .views import SignUpView, ActivateAccount, LoginView, HomeView, LogoutView
+
+app_name = 'account'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('activate/<uidb64>/<token>/', ActivateAccount.as_view(), name='activate'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', HomeView.as_view(), name='home'),
+
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import View, UpdateView
-from .forms import SignUpForm
+from django.views.generic import View, UpdateView, TemplateView, FormView
+from .forms import SignUpForm, UserLoginForm
 from django.contrib.auth.models import User
 
 from django.contrib import messages
@@ -11,12 +11,22 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .token import account_activation_token
 
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.utils.encoding import force_str
 
 
-# Sign Up View
+class HomeView(TemplateView):
+    """
+    Used to render homepage
+    """
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
 class SignUpView(View):
     form_class = SignUpForm
     template_name = "signup.html"
@@ -45,7 +55,7 @@ class SignUpView(View):
             messages.success(request, (
                 'Please Confirm your email to complete registration.'))
 
-            return redirect('login')
+            return redirect("account:login")
 
         return render(request, self.template_name, {'form': form})
 
@@ -66,8 +76,36 @@ class ActivateAccount(View):
             user.save()
             login(request, user)
             messages.success(request, ('Your account have been confirmed.'))
-            return redirect('home')
+            return redirect('account:home')
         else:
             messages.warning(request, (
                 'The confirmation link was invalid, possibly because it has already been used.'))
-            return redirect('home')
+            return redirect('account:home')
+
+
+class LoginView(FormView):
+    """
+        Used to manage User Login view
+    """
+    form_class = UserLoginForm
+    template_name = "login.html"
+
+    def form_valid(self, form):
+        user = form.cleaned_data
+        user.backend = 'django.contrib.auth.backends.ModelBackend'
+        login(self.request, user)
+        return redirect("account:login")
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+
+class LogoutView(FormView):
+    """
+        Used to manage User Logout View
+    """
+
+    def get(self, *args, **kwargs):
+        logout(self.request)
+        messages.success(self.request, "User logged out successfully.")
+        return redirect("account:login")
